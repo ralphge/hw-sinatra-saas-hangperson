4b1f8748a6a33226a537cfbd8d49f1bb6bf21de5
ralphge <Ralph.Geerkens@rwth-aachen.de>
2015-11-10 22:55:36 +0000

    Improved error handling

--- app.rb
+++ app.rb
@@ -39,9 +39,13 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    repeated = @game.guess(letter)
-    if !repeated
-      flash[:message] = "You have already used that letter."
+    begin
+      repeated = @game.guess(letter)
+      if !repeated
+        flash[:message] = "You have already used that letter."
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
     end
     redirect '/show'
   end
